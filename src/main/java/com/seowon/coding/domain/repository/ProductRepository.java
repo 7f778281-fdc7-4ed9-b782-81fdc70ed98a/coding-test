0ddf1f5bf8bad7dc8166000bbc6e80cf8814a655
오형상 <vyg1023@gmail.com>
2025-12-26 15:35:25 +0900

    refactor: ProductService#applyBulkPriceChange 개선

--- src/main/java/com/seowon/coding/domain/repository/ProductRepository.java
+++ src/main/java/com/seowon/coding/domain/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.seowon.coding.domain.repository;
 
 import com.seowon.coding.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String name);
     
     List<Product> findByStockQuantityGreaterThan(int minStock);
+
+    @Query("SELECT * FROM Product p WHERE p.id IN :ids")
+    List<Product> findByIds(@Param("ids") List<Long> ids);
 }
