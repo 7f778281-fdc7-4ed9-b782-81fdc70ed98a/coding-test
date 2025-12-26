56da91ddf225783b637960b9d5135bed4e0f507a
승엽 <tmdduqflfl@naver.com>
2025-12-26 15:54:20 +0900

    Feat: 에러코드  상품 정보 찾을수없음 추가

--- src/main/java/com/seowon/coding/exception/ErrorCode.java
+++ src/main/java/com/seowon/coding/exception/ErrorCode.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    INVALID_CATEGORY_NAME("C001","유효하지 않는 카테고리입니다.");
+    INVALID_CATEGORY_NAME("C001","유효하지 않는 카테고리입니다."),
+    NOT_FOUND_PRODUCT("P001", "상품을 찾을수 없습니다.")
+    ;
 
     private final String code;
     private final String message;
