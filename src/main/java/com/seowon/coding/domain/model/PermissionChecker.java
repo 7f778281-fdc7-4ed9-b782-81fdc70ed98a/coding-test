e30cf563f7915c84854aa03993c2ebf621cf971c
오형상 <vyg1023@gmail.com>
2025-12-26 15:45:58 +0900

    최적화: PermissionChecker#hasPermission 개선

--- src/main/java/com/seowon/coding/domain/model/PermissionChecker.java
+++ src/main/java/com/seowon/coding/domain/model/PermissionChecker.java
@@ -8,7 +8,6 @@ import java.util.List;
 class PermissionChecker {
 
     /**
-     * TODO #7: 코드를 최적화하세요
      * 테스트 코드`PermissionCheckerTest`를 활용하시면 리펙토링에 도움이 됩니다.
      */
     public static boolean hasPermission(
@@ -19,26 +18,46 @@ class PermissionChecker {
             List<UserGroup> groups,
             List<Policy> policies
     ) {
+
+        return checkUser(userId, targetResource, targetAction, users, groups, policies);
+    }
+
+    private static boolean checkUser(String userId, String targetResource, String targetAction, List<User> users, List<UserGroup> groups, List<Policy> policies) {
         for (User user : users) {
             if (user.id.equals(userId)) {
-                for (String groupId : user.groupIds) {
-                    for (UserGroup group : groups) {
-                        if (group.id.equals(groupId)) {
-                            for (String policyId : group.policyIds) {
-                                for (Policy policy : policies) {
-                                    if (policy.id.equals(policyId)) {
-                                        for (Statement statement : policy.statements) {
-                                            if (statement.actions.contains(targetAction) &&
-                                                statement.resources.contains(targetResource)) {
-                                                return true;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                if (checkGroup(targetResource, targetAction, groups, policies, user)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkGroup(String targetResource, String targetAction, List<UserGroup> groups, List<Policy> policies, User user) {
+        for (String groupId : user.groupIds) {
+            for (UserGroup group : groups) {
+                if (group.id.equals(groupId)) {
+                    for (String policyId : group.policyIds) {
+                        if (checkPolicy(targetResource, targetAction, policies, policyId)) return true;
                     }
                 }
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkPolicy(String targetResource, String targetAction, List<Policy> policies, String policyId) {
+        for (Policy policy : policies) {
+            if (policy.id.equals(policyId)) {
+                if (checkStatements(targetResource, targetAction, policy)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkStatements(String targetResource, String targetAction, Policy policy) {
+        for (Statement statement : policy.statements) {
+            if (statement.actions.contains(targetAction) &&
+                statement.resources.contains(targetResource)) {
+                return true;
             }
         }
         return false;
